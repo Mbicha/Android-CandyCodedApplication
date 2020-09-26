f2f8d0b2601004221c518ce832ee0eb0c620e588
Mbithi <mbithicharlse@gmail.com>
2020-09-26 20:17:56 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -26,7 +26,9 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
-
+    private void createMapIntent(){
+        
+    }
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
